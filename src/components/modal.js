ea2c7734f53ffd00fd3168b5a5c78a2e4fab17bb
Maria Chernova <ma4erme@gmail.com>
2022-09-11 23:33:35 +0400

    closeByEscape

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,15 +1,18 @@
+function closeByEscape(evt) {
+  if (evt.key === 'Escape') {
+    const openedPopup = document.querySelector('.popup_opened');
+    closePopup(openedPopup);
+  }
+}
+
 function openPopup(modal) {
   modal.classList.add('popup_opened');
-  window.onkeydown = (evt) => {
-    if (evt.keyCode === 'Escape') {
-      closePopup(modal);
-    }
-  };
+  document.addEventListener('keydown', closeByEscape);
 }
 
 function closePopup(modal) {
   modal.classList.remove('popup_opened');
-  window.onkeydown = null;
+  document.removeEventListener('keydown', closeByEscape);
 }
 
 export { openPopup, closePopup }
